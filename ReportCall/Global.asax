69972c40e965ae3405aa46e1fadcf76a6a762f0b
Pham Duc Minh <phamducminh1990@gmail.com>
2016-11-07 21:18:15 +0700

    Add project files.

---
+++ ReportCall/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReportCall.MvcApplication" Language="C#" %>
---
+++ ReportCall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReportCall.Startup))]
+namespace ReportCall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
